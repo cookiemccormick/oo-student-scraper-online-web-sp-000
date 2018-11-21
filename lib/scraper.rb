8fccb21c9d47103f28c712887ad9fad26d9ebe66
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-11-21 19:13:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'nokogiri'
 
 class Scraper
 
-  def self.scrape_index_page('fixtures/student-site/index.html')
-    html = File.read('fixtures/student-site/index.html')
+  def self.scrape_index_page('../fixtures/student-site/index.html')
+    html = File.read('../fixtures/student-site/index.html')
     students = Nokogiri::HTML(html)
 
     student_profiles = []
